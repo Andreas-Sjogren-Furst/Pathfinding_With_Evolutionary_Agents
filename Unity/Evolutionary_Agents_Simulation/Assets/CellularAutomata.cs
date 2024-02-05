62ab055117a5eb549372858530bc5ccd28ec22d9
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2024-02-05 13:57:46 +0100

    Noise Generation of Map

--- Unity/Evolutionary_Agents_Simulation/Assets/CellularAutomata.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/CellularAutomata.cs
@@ -4,66 +4,56 @@ using UnityEngine;
 
 public class CellularAutomata : MonoBehaviour
 {
-
     public GameObject MapWall;
     public int MapSize;
     public int tileSize;
-    public float densityNoise;
-    
-    private Vector3 position;
-    private int mapTileAmount;
-   
+    public float density; // Add a density field to control noise
 
+    private int mapTileAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        int[,] Map = init2dMap(MapSize, tileSize, densityNoise);
-
-        Debug.Log(mapTileAmount);
-
+        int[,] Map = init2dMap(MapSize, tileSize, density); // Pass density here
 
         for (int i = 0; i < mapTileAmount; i++)
         {
-
             for (int j = 0; j < mapTileAmount; j++)
             {
-                if (Map[i,j] == 1)
+                if (Map[i, j] == 1) // Assuming 1 represents wall
                 {
-                    Instantiate(MapWall, new Vector3(i, 0, j), transform.rotation);
-
+                    Instantiate(MapWall, new Vector3(i * tileSize, 0, j * tileSize), Quaternion.identity);
                 }
-
-
             }
         }
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     int[,] init2dMap(int MapSize, int tileSize, float density)
     {
         mapTileAmount = MapSize / tileSize;
         int[,] Map = new int[mapTileAmount, mapTileAmount];
+        Map = generateNoise(Map, density);
+        return Map;
+    }
 
+    int[,] generateNoise(int[,] map, float density)
+    {
         for (int i = 0; i < mapTileAmount; i++)
         {
             for (int j = 0; j < mapTileAmount; j++)
             {
-                float random = UnityEngine.Random.Range(0f, 100f); // Using UnityEngine's Random
-                Map[i, j] = random > density ? 0 : 1; // Assuming 0 is floor and 1 is wall
+                float randomValue = UnityEngine.Random.Range(0f, 100f); // Generate a random value
+                map[i, j] = randomValue > density ? 0 : 1; // Assign wall or floor based on density
             }
         }
-
-        return Map;
+        return map;
     }
 
-
+    
 }
