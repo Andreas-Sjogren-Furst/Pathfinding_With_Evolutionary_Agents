1719b0967c2d800b72248df75a10ba129fc41259
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2024-02-05 13:55:04 +0100

    Add Noise

--- Unity/Evolutionary_Agents_Simulation/Assets/CellularAutomata.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/CellularAutomata.cs
@@ -8,6 +8,7 @@ public class CellularAutomata : MonoBehaviour
     public GameObject MapWall;
     public int MapSize;
     public int tileSize;
+    public float densityNoise;
     
     private Vector3 position;
     private int mapTileAmount;
@@ -17,7 +18,8 @@ public class CellularAutomata : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[,] Map = init2dMap(MapSize, tileSize);
+
+        int[,] Map = init2dMap(MapSize, tileSize, densityNoise);
 
         Debug.Log(mapTileAmount);
 
@@ -27,15 +29,9 @@ public class CellularAutomata : MonoBehaviour
 
             for (int j = 0; j < mapTileAmount; j++)
             {
-                if (Map[i,j] == 0)
+                if (Map[i,j] == 1)
                 {
                     Instantiate(MapWall, new Vector3(i, 0, j), transform.rotation);
-
-
-                }
-                else
-                {
-
 
                 }
 
@@ -52,11 +48,22 @@ public class CellularAutomata : MonoBehaviour
         
     }
 
-    int[,] init2dMap(int MapSize, int tileSize)
+    int[,] init2dMap(int MapSize, int tileSize, float density)
     {
         mapTileAmount = MapSize / tileSize;
+        int[,] Map = new int[mapTileAmount, mapTileAmount];
 
-        return new int[mapTileAmount, mapTileAmount];
+        for (int i = 0; i < mapTileAmount; i++)
+        {
+            for (int j = 0; j < mapTileAmount; j++)
+            {
+                float random = UnityEngine.Random.Range(0f, 100f); // Using UnityEngine's Random
+                Map[i, j] = random > density ? 0 : 1; // Assuming 0 is floor and 1 is wall
+            }
+        }
+
+        return Map;
     }
+
 
 }
