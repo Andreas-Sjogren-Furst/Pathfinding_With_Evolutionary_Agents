491ee966e9a3acd5c00cb87fc015c93e6a0f29cd
Andreas-Sjogren-Furst <91537952+Andreas-Sjogren-Furst@users.noreply.github.com>
2024-02-16 15:13:58 +0100

    Implemented colony spawner and position of spawner

--- Unity/Evolutionary_Agents_Simulation/Assets/ObjectDropper.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/ObjectDropper.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjectDropper : MonoBehaviour
 {
-    public GameObject objectToDrop; // Assign your prefab in the Inspector
+    public GameObject pheromone; // Assign your prefab in the Inspector
+    public GameObject colony; // Assign your prefab in the Inspector
     private Camera cam;
     void Start(){
         cam = GetComponent<Camera>();
@@ -13,17 +15,19 @@ public class ObjectDropper : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Check if the left mouse button was clicked
-        {
-            DropObjectAtMousePosition();
-        }
+        {   
+            DropObjectAtMousePosition(pheromone);
+        } else if(Input.GetMouseButtonDown(1)){ // Check if the right mouse button was clicked
+            DropObjectAtMousePosition(colony);
+        }   transform.position = Input.mousePosition;
     }
 
-    void DropObjectAtMousePosition()
+    void DropObjectAtMousePosition(GameObject gameObject)
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit)) // Check if the ray hits something
         {
-            Instantiate(objectToDrop, hit.point, Quaternion.identity); // Instantiate the object at the hit position
+            Instantiate(gameObject, hit.point, Quaternion.identity); // Instantiate the object at the hit position
         }
     }
 }
