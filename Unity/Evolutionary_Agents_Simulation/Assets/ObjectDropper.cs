f6b6c9a8e53e8562a42c59005717236e6f360864
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2024-02-16 16:30:30 +0100

    Path finding function between checkpoints

--- Unity/Evolutionary_Agents_Simulation/Assets/ObjectDropper.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/ObjectDropper.cs
@@ -8,20 +8,23 @@ public class ObjectDropper : MonoBehaviour
 {
     public GameObject pheromone; // Assign your prefab in the Inspector
     public GameObject colony;
-    List<Vector3> colonyPositions;
-     // Assign your prefab in the Inspector
+    public List<Vector3> colonyPositions;
+    // Assign your prefab in the Inspector
     private Camera cam;
     private Ray ray;
-    void Start(){
+    void Start()
+    {
         colonyPositions = new List<Vector3>();
         cam = GetComponent<Camera>();
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Check if the left mouse button was clicked
-        {   
+        {
             DropObjectAtMousePosition(pheromone);
-        } else if(Input.GetMouseButtonDown(1)){ // Check if the right mouse button was clicked
+        }
+        else if (Input.GetMouseButtonDown(1))
+        { // Check if the right mouse button was clicked
             DropObjectAtMousePosition(colony);
         }
     }
@@ -29,7 +32,7 @@ public class ObjectDropper : MonoBehaviour
     void DropObjectAtMousePosition(GameObject gameObject)
     {
         ray = cam.ScreenPointToRay(Input.mousePosition);
-        
+
         if (Physics.Raycast(ray, out RaycastHit hit)) // Check if the ray hits something
         {
             colonyPositions.Add(hit.point);
