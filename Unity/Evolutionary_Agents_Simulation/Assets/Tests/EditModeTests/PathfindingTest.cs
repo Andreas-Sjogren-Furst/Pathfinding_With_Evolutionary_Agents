43a09df15aedc15dcdd6ec6a24a2225bc21333b7
Gustavclausen1804 <73382996+Gustavclausen1804@users.noreply.github.com>
2024-05-23 15:17:57 +0200

    Resarch question export data

--- Unity/Evolutionary_Agents_Simulation/Assets/Tests/EditModeTests/PathfindingTest.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/Tests/EditModeTests/PathfindingTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
@@ -22,10 +23,10 @@ public class PathfindingTest
             iterations: 100,
             densityRange: (min: 45, max: 65),
             checkPointsRange: (min: 1, max: 5),
-            cellularIterationsRange: (min: 10, max: 10),
+            cellularIterationsRange: (min: 5, max: 10),
             heightRange: (min: 100, max: 100),
             widthRange: (min: 100, max: 100),
-            name: "density"
+            name: "general_maps_timer"
         );
         // Create instances of the necessary classes
 
@@ -114,11 +115,10 @@ public class PathfindingTest
     private static void RunHPASimulation(int iterations, (int min, int max) densityRange, (int min, int max) checkPointsRange, (int min, int max) cellularIterationsRange, (int min, int max) heightRange, (int min, int max) widthRange, string name)
     {
         var random = new System.Random();
-        var results = new List<string> { "Iteration,Density,CheckPoints,CellularIterations,Height,Width,PathLength_Level1,NodesExplored_Level1,PathLength_Level2,NodesExplored_Level2,PathLength_Level3,NodesExplored_Level3,PathLength_AStar,NodesExplored_AStar" };
+        var results = new List<string> { "Iteration,Density,CheckPoints,CellularIterations,Height,Width,PathLength_Level1,NodesExplored_Level1,Time_Level1,PathLength_Level2,NodesExplored_Level2,Time_Level2,PathLength_Level3,NodesExplored_Level3,Time_Level3,PathLength_AStar,NodesExplored_AStar,Time_AStar" };
 
         for (int i = 0; i < iterations; i++)
         {
-
             int density = random.Next(densityRange.min, densityRange.max);
             int numberOfCheckPoints = random.Next(checkPointsRange.min, checkPointsRange.max);
             int cellularIterations = random.Next(cellularIterationsRange.min, cellularIterationsRange.max);
@@ -131,30 +131,36 @@ public class PathfindingTest
             Vector2Int end = new Vector2Int(mapModel.height - 10, mapModel.width - 10);
 
             // HPA* Pathfinding at different abstraction levels
-            HPAPath hpaPath1 = EffiencyInNodeExploration(tileMap, start, end, 1, RefinePath: false);
-            HPAPath hpaPath2 = EffiencyInNodeExploration(tileMap, start, end, 2, RefinePath: false);
-            HPAPath hpaPath3 = EffiencyInNodeExploration(tileMap, start, end, 3, RefinePath: false);
+            (HPAPath hpaPath1, var timeLevel1) = EffiencyInNodeExploration(tileMap, start, end, 1, RefinePath: false);
+
+            (HPAPath hpaPath2, var timeLevel2) = EffiencyInNodeExploration(tileMap, start, end, 2, RefinePath: false);
+
+            (HPAPath hpaPath3, var timeLevel3) = EffiencyInNodeExploration(tileMap, start, end, 3, RefinePath: false);
 
             // A* Pathfinding
+            var stopwatch = Stopwatch.StartNew();
             (List<Vector2Int> Path, int NodesExplored) Apath = Astar.FindPath(start, end, tileMap);
-
-
-
+            stopwatch.Stop();
+            var timeAStar = stopwatch.ElapsedMilliseconds;
 
             string result = $"{i + 1}," +
-                       $"{density}," +
-                       $"{numberOfCheckPoints}," +
-                       $"{cellularIterations}," +
-                       $"{height}," +
-                       $"{width}," +
-                       $"{(hpaPath1?.path?.Count ?? 0)}," +
-                       $"{(hpaPath1?.NodesExplored ?? 0)}," +
-                       $"{(hpaPath2?.path?.Count ?? 0)}," +
-                       $"{(hpaPath2?.NodesExplored ?? 0)}," +
-                       $"{(hpaPath3?.path?.Count ?? 0)}," +
-                       $"{(hpaPath3?.NodesExplored ?? 0)}," +
-                       $"{(Apath.Path?.Count ?? 0)}," +
-                       $"{(Apath.NodesExplored)}";
+                            $"{density}," +
+                            $"{numberOfCheckPoints}," +
+                            $"{cellularIterations}," +
+                            $"{height}," +
+                            $"{width}," +
+                            $"{hpaPath1?.path?.Count ?? 0}," +
+                            $"{hpaPath1?.NodesExplored ?? 0}," +
+                            $"{timeLevel1}," +
+                            $"{hpaPath2?.path?.Count ?? 0}," +
+                            $"{hpaPath2?.NodesExplored ?? 0}," +
+                            $"{timeLevel2}," +
+                            $"{hpaPath3?.path?.Count ?? 0}," +
+                            $"{hpaPath3?.NodesExplored ?? 0}," +
+                            $"{timeLevel3}," +
+                            $"{Apath.Path?.Count ?? 0}," +
+                            $"{Apath.NodesExplored}," +
+                            $"{timeAStar}";
 
             results.Add(result);
         }
@@ -162,12 +168,11 @@ public class PathfindingTest
 
         string filePath = Path.Combine(Application.dataPath, name);
         File.WriteAllLines(filePath, results);
-        Debug.Log($"Results saved to {filePath}");
-
+        UnityEngine.Debug.Log($"Results saved to {filePath}");
     }
 
 
-    public static HPAPath EffiencyInNodeExploration(int[,] tileMap, Vector2Int start, Vector2Int end, int maxLevel, Boolean RefinePath = false)
+    public static (HPAPath, long) EffiencyInNodeExploration(int[,] tileMap, Vector2Int start, Vector2Int end, int maxLevel, Boolean RefinePath = false)
     {
         PathFinder _pathFinder = new PathFinder();
         GraphModel _graphModel = new GraphModel(tileMap);
@@ -180,13 +185,18 @@ public class PathfindingTest
         HPAStar.Preprocessing(maxLevel);
         if (RefinePath)
         {
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
             HPAPath hpaPath = HPAStar.HierarchicalSearch(start, end, maxLevel);
-            return hpaPath;
+            stopwatch.Stop();
+            return (hpaPath, stopwatch.ElapsedMilliseconds);
         }
         else
         {
+            Stopwatch stopwatch = Stopwatch.StartNew();
             HPAPath hpaPath = HPAStar.HierarchicalAbstractSearch(start, end, maxLevel);
-            return hpaPath;
+            stopwatch.Stop();
+            return (hpaPath, stopwatch.ElapsedMilliseconds);
         }
 
     }
