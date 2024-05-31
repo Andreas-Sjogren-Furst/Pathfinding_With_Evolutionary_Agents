eda9b118393908ce574370a3ebcaf5e3b29da3cc
Andreas-Sjogren-Furst <91537952+Andreas-Sjogren-Furst@users.noreply.github.com>
2024-05-31 15:20:23 +0200

    Update CameraAdjuster.cs

--- Unity/Evolutionary_Agents_Simulation/Assets/UI/CameraAdjuster.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/UI/CameraAdjuster.cs
@@ -15,7 +15,12 @@ public class CameraAdjuster : MonoBehaviour
     public void AdjustCamera(){
         ScreenViewModel screenViewModel = WebView.Instance.screenPresenter.PackageData();
         mapSize = screenViewModel.map.GetLength(0)/2 + frameSize;
+        int x = screenViewModel.spawnPoint.ArrayPosition.x;
+        int z = screenViewModel.spawnPoint.ArrayPosition.y;
+        int y = 50;
+        Vector3 cameraPosition = new Vector3(x,y,z);
         Camera.main.orthographicSize = mapSize;
+        Camera.main.transform.position = cameraPosition;
     }
 
     
