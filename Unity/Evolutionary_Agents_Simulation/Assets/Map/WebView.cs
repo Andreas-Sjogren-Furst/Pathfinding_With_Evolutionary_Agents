aa72c153c816a13c96ca2a0ac2d0e7924e6c933a
Andreas-Sjogren-Furst <91537952+Andreas-Sjogren-Furst@users.noreply.github.com>
2024-06-05 19:36:53 +0200

    Increased checkpoint amount

--- Unity/Evolutionary_Agents_Simulation/Assets/Map/WebView.cs
+++ Unity/Evolutionary_Agents_Simulation/Assets/Map/WebView.cs
@@ -114,6 +114,7 @@ public class WebView : MonoBehaviour, IScreenView
             myGameManager.agentController.SimulateAgents();
             MoveAgents();
             RenderFrontiers();
+            
         }
     }
     public void CreateNewMap(MapModel mapModel)
